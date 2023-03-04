03e9ad95b7674e3ce2d6836fe86724233ec20bfa
Murilo Manente <manentemurilo@gmail.com>
2023-03-04 18:22:23 -0300

    feat: dynamic carousel indicators

--- ebuy.app/src/app/pages/home/home-component/home.component.ts
+++ ebuy.app/src/app/pages/home/home-component/home.component.ts
@@ -9,21 +9,21 @@ import { Image } from 'src/app/models/image-carousel';
 })
 export class HomeComponent {
   images: Image[] = [
-    { 
+    {
       url: '../../../assets/online-shopping_banner-1600x500.jpg',
-      interval: 10000,
+      interval: 5000,
       alt: 'Uma imagem com a cor de fundo amaréla com algumas figuras de celular remetendo a compras online.',
       active: true
     },
-    { 
+    {
       url: '../../../assets/discount-50-percent_banner-1600x500.jpg',
-      interval: 2000,
+      interval: 5000,
       alt: 'Imagem com a cor de fundo vermelha dizendo que estamos com 50% de desconto nos nossos produtos.',
       active: false
     },
-    { 
+    {
       url: '../../../assets/cashback_banner-1600x500.jpg',
-      interval: 0,
+      interval: 5000,
       alt: 'Imagem com a cor de fundo azul com alguém segurando um celular dizendo que temos cashback em nossas vendas.',
       active: false
     }
