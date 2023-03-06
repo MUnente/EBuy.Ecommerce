5aee205e82e8e8dbc55f105ed8a761a94ae4e992
Murilo Manente <murilo.manente@targetsoftware.com.br>
2023-03-06 16:08:31 -0300

    feat: [base] image slider on product page

--- ebuy.app/src/app/pages/products/product-details/product-details.component.ts
+++ ebuy.app/src/app/pages/products/product-details/product-details.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent { }
+export class ProductDetailsComponent {
+  constructor(private elementRef: ElementRef) {}
+  
+  changeImg(image: any): void {
+    this.elementRef.nativeElement.querySelector('.img-expand > img').src = image.getAttribute('src');
+  }
+}
