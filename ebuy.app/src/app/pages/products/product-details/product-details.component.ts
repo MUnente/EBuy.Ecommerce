6571241c03213e6e45108d595e1cbd6c19b16555
Murilo Manente <murilo.manente@targetsoftware.com.br>
2023-03-06 18:05:31 -0300

    feat: tiny-slider for vertical slider on product page

--- ebuy.app/src/app/pages/products/product-details/product-details.component.ts
+++ ebuy.app/src/app/pages/products/product-details/product-details.component.ts
@@ -1,14 +1,29 @@
-import { Component, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { tns } from 'tiny-slider';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements AfterViewInit {
   constructor(private elementRef: ElementRef) {}
   
   changeImg(image: any): void {
     this.elementRef.nativeElement.querySelector('.img-expand > img').src = image.getAttribute('src');
+  }
+
+  ngAfterViewInit(): void {
+    tns({
+      container: '.product-image-list',
+      items: 4,
+      axis: 'vertical',
+      swipeAngle: true,
+      speed: 400,
+      loop: false,
+      nav: false,
+      prevButton: '.fa-arrow-up.button-arrow',
+      nextButton: '.fa-arrow-down.button-arrow',
+    });
   }
 }
