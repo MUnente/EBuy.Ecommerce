83a42b5c14ed4e4103206baa8ac3088f945f0fe3
Murilo Manente <murilo.manente@targetsoftware.com.br>
2023-03-09 12:45:04 -0300

    refactor: app logo component on auth page

--- ebuy.app/src/app/pages/auth/auth.module.ts
+++ ebuy.app/src/app/pages/auth/auth.module.ts
@@ -3,18 +3,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { TermsModalModule } from 'src/app/components/terms-modal/terms-modal.module';
 
 import { LoginComponent } from './login-component/login.component';
 import { RegisterComponent } from './register-component/register.component';
 
 import { AuthService } from 'src/app/services/auth.service';
+import { LogoCenterComponent } from './components/logo-center/logo-center.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LogoCenterComponent
   ],
   imports: [
     CommonModule,
@@ -22,7 +23,6 @@ import { AuthService } from 'src/app/services/auth.service';
     ReactiveFormsModule,
     AuthRoutingModule,
     TermsModalModule,
-    SharedModule
   ],
   providers: [
     AuthService
